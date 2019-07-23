ca4fc36c4c51e08974b09401b49f975198c3166c
spaaam <53124531+spaaam@users.noreply.github.com>
2019-07-23 21:31:59 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("590858239596560394")
+setInterval(function() {
+channel.send(`Credit Spam , Frame , Credit By Kahrba , Farm Credit By Kahrba, ez Farm - كردت اسبام اسبام اسبام اوف ءف اح اح نار ج [ " 248 " ]`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
